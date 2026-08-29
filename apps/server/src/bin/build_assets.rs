2673132d16d685d19407c8152a77608b823cf4e1
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-30 00:00:25 +0530

    Add few checks for asset and dist paths

--- apps/server/src/bin/build_assets.rs
+++ apps/server/src/bin/build_assets.rs
@@ -1,21 +1,27 @@
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use std::fs;
 use std::path::{Path, PathBuf};
 
+const ASSETS_DIR: &str = "assets";
+const DIST_DIR: &str = "dist";
 const SKIP_EXTENSIONS: [&str; 1] = ["map"];
 
-fn process_and_copy_asset(path: &PathBuf, to_hash: bool) -> anyhow::Result<()> {
-    let base_path = path.with_extension("");
+fn process_and_copy_asset(asset_path: &PathBuf, to_hash: bool) -> anyhow::Result<()> {
+    // take path and open file buffer
+    let base_path = asset_path.with_extension("");
     let base_path = base_path.to_str().context("Invalid UTF-8 in filename")?;
-    let ext = path
+    let ext = asset_path
         .extension()
-        .context("Asset path has no extension")?
+        .context(format!(
+            "Asset {} path has no extension",
+            asset_path.display()
+        ))?
         .to_str()
         .context("Invalid UTF-8 chars in extension")?;
-    // take path and open file buffer
-    let data = fs::read(path).context(format!("Failed reading file at {}", path.display()))?;
+    let data =
+        fs::read(asset_path).context(format!("Failed reading file at {}", asset_path.display()))?;
     // take hash of the content of the file using blake3
-    let source_file_path: String = if to_hash {
+    let dest_relative_path: String = if to_hash {
         let hex_str = blake3::hash(&data).to_hex();
         let short_hash = hex_str
             .as_str()
@@ -26,10 +32,14 @@ fn process_and_copy_asset(path: &PathBuf, to_hash: bool) -> anyhow::Result<()> {
         format!("{base_path}.{ext}")
     };
     // In the outdir dump file's data that we read earlier with same hierarchy but with hash in its name
-    let dest_path = PathBuf::from("dist").join(&source_file_path);
-    println!("path={}, dest_path={}", path.display(), dest_path.display());
+    let dest_full_path = PathBuf::from(DIST_DIR).join(&dest_relative_path);
+    println!(
+        "Building asset: asset_path={}, dest_full_path={}",
+        asset_path.display(),
+        dest_full_path.display()
+    );
 
-    let dest_dir = dest_path
+    let dest_dir = dest_full_path
         .parent()
         .context("Destination path has no parent directory")?;
 
@@ -40,57 +50,90 @@ fn process_and_copy_asset(path: &PathBuf, to_hash: bool) -> anyhow::Result<()> {
         )
     })?;
 
-    fs::write(&dest_path, &data)
-        .with_context(|| format!("Failed writing asset to {}", dest_path.display()))?;
+    fs::write(&dest_full_path, &data)
+        .with_context(|| format!("Failed writing asset to {}", dest_full_path.display()))?;
 
     Ok(())
 }
 
 fn should_skip(path: &Path) -> anyhow::Result<bool> {
-    if let Some(ext) = path.extension()
-        && SKIP_EXTENSIONS.contains(&ext.to_str().context("Invalid UTF-8 in extension")?)
-    {
-        return Ok(true);
-    }
-    Ok(path.ends_with(".DS_Store"))
+    path.extension().map_or_else(
+        // None option will be for extensionless files like MacOS .DS_Store
+        || Ok(true),
+        |ext| {
+            if SKIP_EXTENSIONS.contains(&ext.to_str().context("Invalid UTF-8 in extension")?) {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        },
+    )
 }
 
-fn build_assets(dir: &PathBuf) -> anyhow::Result<()> {
-    if dir.is_file() {
-        if should_skip(dir)? {
+fn is_vendor_asset(asset_path: &Path) -> anyhow::Result<bool> {
+    let vendor_path = Path::new(ASSETS_DIR).join("vendor");
+    Ok(asset_path.starts_with(
+        vendor_path
+            .to_str()
+            .context(format!("Invalid vendor path: {}", vendor_path.display()))?,
+    ))
+}
+
+fn build_assets(path: &PathBuf) -> anyhow::Result<()> {
+    if path.is_file() {
+        if should_skip(path)? {
             return Ok(());
         }
-        let to_hash = !dir
-            .to_str()
-            .context("Error trying to read pathname")?
-            .contains("vendor");
-        process_and_copy_asset(dir, to_hash)?;
+        let to_hash =
+            !is_vendor_asset(path).context(format!("Invalid path: {}", path.display()))?;
+        process_and_copy_asset(path, to_hash)?;
     }
-    for entry in fs::read_dir(dir)? {
-        let path = entry?.path();
-        if path.is_file() {
-            if should_skip(&path)? {
-                continue;
-            }
-            let to_hash = !path
-                .to_str()
-                .context("Error trying to read pathname")?
-                .contains("vendor");
-            process_and_copy_asset(&path, to_hash)?;
+    for entry in fs::read_dir(path)? {
+        let asset_path = entry?.path();
+        if asset_path.is_dir() {
+            build_assets(&asset_path)?;
             continue;
         }
-        build_assets(&path)?;
+        if should_skip(&asset_path)? {
+            continue;
+        }
+        let to_hash = !is_vendor_asset(&asset_path)
+            .context(format!("Invalid asset_path: {}", asset_path.display()))?;
+        process_and_copy_asset(&asset_path, to_hash)?;
     }
     Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
-    let dist_dir = Path::new("dist");
+    let assets_path = PathBuf::from(ASSETS_DIR);
+    if !assets_path.exists() {
+        println!(
+            "No assets dir '{}' in current directory found.",
+            assets_path.display()
+        );
+        return Err(anyhow!(format!("No assets dir '{}' in current directory found.",
+            assets_path.display())))
+    }
+    if !assets_path.is_dir() {
+        return Err(anyhow!(format!("Assets path '{}' is a file and not a dir",
+            assets_path.display())))
+    }
+
+    let dist_dir = assets_path
+        .parent()
+        .context(format!(
+            "Invalid assets parent dir assets_path={}",
+            assets_path.display()
+        ))?
+        .join(DIST_DIR);
     if dist_dir.exists() {
-        fs::remove_dir_all(dist_dir)?;
-        println!("Cleaned existing dist/ dir");
+        if !dist_dir.is_dir() {
+            return Err(anyhow!(format!("dist_dir={} exists and is a file", dist_dir.display())));
+        }
+        fs::remove_dir_all(&dist_dir)?;
+        println!("Cleaned existing {}", dist_dir.display());
     }
     fs::create_dir_all(dist_dir)?;
-    build_assets(&PathBuf::from("assets"))?;
+    build_assets(&PathBuf::from(ASSETS_DIR))?;
     Ok(())
 }
